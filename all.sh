7a8a6bb7df4900f127443ffb565a8591437f2988
PandaNinjas <admin@malwarefight.wip.la>
2023-05-18 17:43:12 -0400

    Don't build python2

--- all.sh
+++ all.sh
@@ -1,1 +1,1 @@
-curl https://www.python.org/ftp/python/ 2>/dev/null | grep -oP '\d+\.\d+\.\d+' | xargs ./main.sh
+curl https://www.python.org/ftp/python/ 2>/dev/null | grep -oP '\d+\.\d+\.\d+' | grep -v '2.' | xargs ./main.sh
